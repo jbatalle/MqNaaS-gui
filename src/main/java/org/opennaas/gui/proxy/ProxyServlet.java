235c87913ae79913f9ab29b9396ef8e2ef52ad0e
Josep <jbatalle3@gmail.com>
2014-12-11 16:32:45 +0100

    Preapre POST method in proxy

--- src/main/java/org/opennaas/gui/proxy/ProxyServlet.java
+++ src/main/java/org/opennaas/gui/proxy/ProxyServlet.java
@@ -7,12 +7,13 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.util.Map.Entry;
+import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.opennaas.gui.generic.utils.Constants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,15 +26,17 @@ public class ProxyServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private final String USER_AGENT = "Mozilla/5.0";
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final String baseUrl = Constants.WS_REST_URL;
 
     public ProxyServlet() {
         super();
     }
 
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //  Create Get request dynamically to remote server
         String url = "http://ipaddress:port/contextpath" + request.getAttribute("uri") + "?" + request.getQueryString();
-        url = "http://admin:123456@localhost:9000"+request.getAttribute("path");//recevie /mqnaas/IRootResourceProvider
+        url = baseUrl + request.getAttribute("path");//recevie /mqnaas/IRootResourceProvider
         URL obj = new URL(url);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
@@ -50,7 +53,7 @@ public class ProxyServlet extends HttpServlet {
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(con.getInputStream()));
         String inputLine;
-        StringBuffer response1 = new StringBuffer();
+        StringBuilder response1 = new StringBuilder();
 
         ServletOutputStream sout = response.getOutputStream();
 
@@ -63,59 +66,59 @@ public class ProxyServlet extends HttpServlet {
         sout.flush();
 
     }
-    /*
-     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-     //  Create Post request dynamically to remote server
-     String url = "http://ipaddress:port/contextpath"+request.getAttribute("uri");
-        
-     URL obj = new URL(url);
-     HttpURLConnection con = (HttpURLConnection) obj.openConnection();
- 
-     //add reuqest header
-     con.setRequestMethod("POST");
-     con.setRequestProperty("User-Agent", USER_AGENT);
-     con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
- 
-     StringBuilder sb = new StringBuilder();
-     for(Entry<String, String[]> e : request.getParameterMap().entrySet()){
-     if(sb.length() > 0){
-     sb.append('&');
-     }
-     String[] temp =e.getValue();
-     for(String s:temp) {
-     sb.append(URLEncoder.encode(e.getKey(), "UTF-8")).append('=').append(URLEncoder.encode(s, "UTF-8"));  
-     }
-     }
-        
-     String urlParameters = sb.toString();
- 
-        
-     // Send post request
-     con.setDoOutput(true);
-     DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-     wr.writeBytes(urlParameters);
-     wr.flush();
-     wr.close();
- 
-     int responseCode = con.getResponseCode();
-     System.out.println("\nSending 'POST' request to URL : " + url);
-     System.out.println("Post parameters : " + urlParameters);
-     System.out.println("Response Code : " + responseCode);
- 
-     BufferedReader in = new BufferedReader(
-     new InputStreamReader(con.getInputStream()));
-     String inputLine;
-     StringBuffer response1 = new StringBuffer();
- 
-     ServletOutputStream sout = response.getOutputStream();
-        
-     while ((inputLine = in.readLine()) != null) {
-     response1.append(inputLine);
-     sout.write(inputLine.getBytes());
-     }
-     in.close();
- 
-     sout.flush();
-     }
-     */
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //  Create Post request dynamically to remote server
+        String url = "http://ipaddress:port/contextpath" + request.getAttribute("uri");
+
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        //add reuqest header
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<String, String[]> e : ((Map<String, String[]>) request.getParameterMap()).entrySet()) {
+//     for(Entry<String, String[]> e : request.getParameterMap().entrySet()){
+            if (sb.length() > 0) {
+                sb.append('&');
+            }
+            String[] temp = e.getValue();
+            for (String s : temp) {
+                sb.append(URLEncoder.encode(e.getKey(), "UTF-8")).append('=').append(URLEncoder.encode(s, "UTF-8"));
+            }
+        }
+
+        String urlParameters = sb.toString();
+
+        // Send post request
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(urlParameters);
+        wr.flush();
+        wr.close();
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'POST' request to URL : " + url);
+        System.out.println("Post parameters : " + urlParameters);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder response1 = new StringBuilder();
+
+        ServletOutputStream sout = response.getOutputStream();
+
+        while ((inputLine = in.readLine()) != null) {
+            response1.append(inputLine);
+            sout.write(inputLine.getBytes());
+        }
+        in.close();
+
+        sout.flush();
+    }
+
 }
