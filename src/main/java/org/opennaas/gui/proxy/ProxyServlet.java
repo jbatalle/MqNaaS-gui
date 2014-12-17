13da81927ec68caa505a5e669a68aad61debb0a6
Josep <jbatalle3@gmail.com>
2014-12-17 15:49:59 +0100

    PUT and DELETE methods implemented

--- src/main/java/org/opennaas/gui/proxy/ProxyServlet.java
+++ src/main/java/org/opennaas/gui/proxy/ProxyServlet.java
@@ -34,9 +34,11 @@ public class ProxyServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("Method req: GET");
         //  Create Get request dynamically to remote server
         String url = "http://ipaddress:port/contextpath" + request.getAttribute("uri") + "?" + request.getQueryString();
         url = baseUrl + request.getAttribute("path");//recevie /mqnaas/IRootResourceProvider
+        logger.error("URL: "+url);
         URL obj = new URL(url);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
@@ -69,9 +71,11 @@ public class ProxyServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.error("MEthod Req: PUT");
         //  Create Post request dynamically to remote server
         String url = "http://ipaddress:port/contextpath" + request.getAttribute("uri");
-
+        url = baseUrl + request.getAttribute("path");//recevie /mqnaas/IRootResourceProvider
+logger.error("URL: "+url);
         URL obj = new URL(url);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
@@ -107,6 +111,99 @@ public class ProxyServlet extends HttpServlet {
         System.out.println("Response Code : " + responseCode);
 
         BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder response1 = new StringBuilder();
+
+        ServletOutputStream sout = response.getOutputStream();
+
+        while ((inputLine = in.readLine()) != null) {
+            response1.append(inputLine);
+            sout.write(inputLine.getBytes());
+        }
+        in.close();
+
+        sout.flush();
+    }
+    
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.error("Method Req: PUT");
+        //  Create Post request dynamically to remote server
+        String url = "http://ipaddress:port/contextpath" + request.getAttribute("uri");
+        url = baseUrl + request.getAttribute("path");//recevie /mqnaas/IRootResourceProvider
+logger.error("URL: "+url);
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        //add reuqest header
+        con.setRequestMethod("PUT");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+        con.setRequestProperty("Content-Type", "application/xml");
+
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<String, String[]> e : ((Map<String, String[]>) request.getParameterMap()).entrySet()) {
+//     for(Entry<String, String[]> e : request.getParameterMap().entrySet()){
+            if (sb.length() > 0) {
+                sb.append('&');
+            }
+            String[] temp = e.getValue();
+            for (String s : temp) {
+                sb.append(URLEncoder.encode(e.getKey(), "UTF-8")).append('=').append(URLEncoder.encode(s, "UTF-8"));
+            }
+        }
+        String urlParameters = sb.toString();
+
+        // Send put request
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(urlParameters);
+        wr.writeBytes((String) request.getAttribute("object"));
+        wr.flush();
+        wr.close();
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'PUT' request to URL : " + url);
+        System.out.println("Post parameters : " + urlParameters);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder response1 = new StringBuilder();
+
+        ServletOutputStream sout = response.getOutputStream();
+
+        while ((inputLine = in.readLine()) != null) {
+            response1.append(inputLine);
+            sout.write(inputLine.getBytes());
+        }
+        in.close();
+
+        sout.flush();
+    }
+    
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("Method req: DELETE");
+        //  Create Get request dynamically to remote server
+        String url = "http://ipaddress:port/contextpath" + request.getAttribute("uri") + "?" + request.getQueryString();
+        url = baseUrl + request.getAttribute("path");//recevie /mqnaas/IRootResourceProvider
+        logger.error("URL: "+url);
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        // optional default is GET
+        con.setRequestMethod("DELETE");
+
+        //add request header
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'DELETE' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
         String inputLine;
         StringBuilder response1 = new StringBuilder();
 
--- src/main/java/org/opennaas/gui/rest/resources/MqNaaSResource.java
+++ src/main/java/org/opennaas/gui/rest/resources/MqNaaSResource.java
@@ -1,21 +1,28 @@
 package org.opennaas.gui.rest.resources;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.Map.Entry;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import org.opennaas.gui.services.RestServiceException;
+import org.opennaas.gui.utils.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Component
@@ -84,4 +91,45 @@ public class MqNaaSResource {
      String response = clientJersey.get(path, req);
      return response;
      }*/
+
+    @PUT
+//    @Produces(MediaType.APPLICATION_XML)
+    @Path("{path}")
+    public @ResponseBody
+    void put(@PathParam("path") String path, @RequestBody Object object, @Context HttpServletRequest request, @Context HttpServletResponse response) throws IOException, RestServiceException, ServletException {
+        this.logger.info("PUT() " + path);
+        String url = "http://admin:123456@localhost:9000/mqnaas/IRootResourceProvider";
+//response.sendRedirect("http://admin:123456@localhost:9000/mqnaas/IRootResourceProvider");
+//        response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
+//response.setHeader("Location", "http://admin:123456@localhost:9000/mqnaas/IRootResourceProvider");
+//response.flushBuffer();
+        logger.info("URI: " + (String) request.getAttribute("uri"));
+        String test = Utils.convertToJson(object);
+        logger.info("Object: " + test);
+        logger.info("Object: " + object.toString());
+        HttpServletRequest req = (HttpServletRequest) request;
+        request.setAttribute("uri", req.getRequestURI().substring(req.getContextPath().length()));
+        request.setAttribute("path", request.getPathInfo());
+//        request.setAttribute("object", object.toString());
+//        String json = '<?xml version="1.0"?>';
+        //xml = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><ns2:rootResourceDescriptor xmlns:ns2=\"org.mqnaas\"><specification><type>NETWORK</type><model>Internal</model><version>1.0</version></specification></ns2:rootResourceDescriptor>";
+        request.setAttribute("object", Utils.adaptJsonToXml(test));
+        // Forward filtered requests to MyProxy servlet
+        request.getRequestDispatcher("/ProxyServlet").forward(request, response);
+
+        return;
+    }
+    
+    @DELETE
+    @Path("{path}/{id}")
+    public void delete(@PathParam("path") String path, @Context HttpServletRequest request, @Context HttpServletResponse response) throws IOException, RestServiceException, ServletException {
+        this.logger.info("DELETE() " + path);
+        String url = "http://admin:123456@localhost:9000/mqnaas/IRootResourceProvider";
+        logger.info("URI: " + (String) request.getAttribute("uri"));
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        request.setAttribute("uri", req.getRequestURI().substring(req.getContextPath().length()));
+        request.setAttribute("path", request.getPathInfo());
+        request.getRequestDispatcher("/ProxyServlet").forward(request, response);
+    }
 }
