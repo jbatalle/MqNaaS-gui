b160fbb03a0eb1f7d879e43d076e499d317ccab7
Josep <jbatalle3@gmail.com>
2014-12-22 18:44:12 +0100

    Some html changes and rootResourceController

--- src/main/webapp/js/controllers/rootResourceController.js
+++ src/main/webapp/js/controllers/rootResourceController.js
@@ -2,8 +2,18 @@
 
 angular.module('openNaaSApp')
         .controller('RootResourceController', function ($scope, RootResourceService, localStorageService) {
-
+console.log(localStorageService.get("mqNaaSElements"));
             RootResourceService.list().then(function (data) {
+                console.log(data);
+                console.log(data.length);
+                if (data.IRootResource.IRootResourceId instanceof Array) {
+                    console.log("IS AN ARRAY");
+                } else {
+                    console.log("IS NOT AN ARRAY");
+                    var el = data.IRootResource.IRootResourceId;
+                    data.IRootResource.IRootResourceId = [el];
+//                    data.IRootResource.IRootResourceId[0] = el;
+                }
                 console.log(data);
                 $scope.data = data;
                 localStorageService.set("mqNaaSElements", data);
@@ -18,14 +28,16 @@ angular.module('openNaaSApp')
                 });
             };
 
-        })
-        .controller('CreateRootResourceController', function ($scope, RootResourceService) {
-            var xml = getNETWORK();
-            console.log(xml);
-            RootResourceService.put(xml).then(function (data) {
-                $scope.data = data;
-                console.log($scope.data);
-            });
+            $scope.createNetwork = function () {
+                console.log("Create network");
+                var xml = getNETWORK();
+                console.log(xml);
+                RootResourceService.put(xml).then(function (data) {
+                    $scope.data = data;
+                    console.log($scope.data);
+                });
+            };
+
         })
         .controller('InfoRootResourceController', function ($scope, RootResourceService, $routeParams, localStorageService) {
             RootResourceService.get($routeParams.id).then(function (data) {
