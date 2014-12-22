250fd0f8770bafcc5faad78f30896dc9f76ada1c
Josep <jbatalle3@gmail.com>
2014-12-22 18:42:26 +0100

    Crawler

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -3,137 +3,145 @@
 angular.module('openNaaSApp', ['ngResource', 'ngRoute', 'ngCookies', 'openNaaSApp.services', 'LocalStorageModule', 'cb.x2js', 'ngTable', 'ngDialog'])
         .config(function (localStorageServiceProvider) {
             localStorageServiceProvider
-                .setPrefix('openNaaSApp')
-                .setStorageType('sessionStorage')
-                .setNotify(true, true);
+                    .setPrefix('openNaaSApp')
+                    .setStorageType('sessionStorage')
+                    .setNotify(true, true);
         }).config(
-                ['$routeProvider', '$locationProvider', '$httpProvider', function ($routeProvider, $locationProvider, $httpProvider) {
+        ['$routeProvider', '$locationProvider', '$httpProvider', function ($routeProvider, $locationProvider, $httpProvider) {
 
-                        $routeProvider
-/*                                .when('/create', {
-                                    templateUrl: 'partials/create.html',
-                                    controller: CreateController
-                                })
-                                .when('/edit/:id', {
-                                    templateUrl: 'partials/edit.html',
-                                    controller: EditController
-                                })
-*/                                .when('/login', {
-                                    templateUrl: 'partials/login.html',
-                                    controller: 'LoginController'
-                                })
-                                .when('/about', {
-                                    templateUrl: 'partials/about.html',
-                                    controller: 'AboutCtrl',
-                                })
-                                .when('/users', {
-                                    templateUrl: 'partials/users.html',
-                                    controller: 'UsersController'
-                                })
-                                .when('/testMq', {
-                                    templateUrl: 'partials/testmq.html',
-                                    controller: 'TestMqController'
-                                })
-                                .when('/history', {
-                                    templateUrl: 'partials/history/index.html',
-                                    controller: 'HistoryListController'
-                                })
-                                .when('/createHistory', {
-                                    templateUrl: 'partials/history/create.html',
-                                    controller: 'HistoryCreateController'
-                                })
-                                .when('/rootResource', {
-                                    templateUrl: 'partials/rootResource/index.html',
-                                    controller: 'RootResourceController'
-                                }).when('/info/:id', {
-                                    templateUrl: 'partials/rootResource/info.html',
-                                    controller: 'InfoRootResourceController'
-                                })
-                                .when('/listServices/:id', {
-                                    templateUrl: 'partials/serviceProvider/info.html',
-                                    controller: 'serviceProviderController'
-                                })
-                                .when('/rootResource/create', {
-                                    templateUrl: 'partials/rootResource/info.html',
-                                    controller: 'CreateRootResourceController'
-                                })
-                                .when('/tree', {
-                                    templateUrl: 'partials/tree.html',
-                                    controller: 'treeCtrl'
-                                })
-                                .when('/infoResource/:id', {
-                                    templateUrl: 'partials/mqnaasResource/info.html',
-                                    controller: 'MqNaaSResourceController'
-                                })
-                                .when('/sodales', {
-                                    templateUrl: 'partials/sodales/index.html',
-                                    controller: 'SodalesController'
-                                })
-                                .when('/sodales_statistics', {
-                                    templateUrl: 'partials/sodales/statistics.html',
-                                    controller: 'SodalesStatisticsController'
-                                })
-                                .when('/graph', {
-                                    templateUrl: 'partials/graph.html'
-                                })
-                                .otherwise({
-                                    templateUrl: 'partials/index.html',
-                                    //controller: IndexController
-                                });
+                $routeProvider
+                        /*                                .when('/create', {
+                         templateUrl: 'partials/create.html',
+                         controller: CreateController
+                         })
+                         .when('/edit/:id', {
+                         templateUrl: 'partials/edit.html',
+                         controller: EditController
+                         })
+                         */.when('/login', {
+                            templateUrl: 'partials/login.html',
+                            controller: 'LoginController'
+                        })
+                        .when('/about', {
+                            templateUrl: 'partials/about.html',
+                            controller: 'AboutCtrl'
+                        })
+                        .when('/users', {
+                            templateUrl: 'partials/users.html',
+                            controller: 'UsersController'
+                        })
+                        .when('/testMq', {
+                            templateUrl: 'partials/testmq.html',
+                            controller: 'TestMqController'
+                        })
+                        .when('/history', {
+                            templateUrl: 'partials/history/index.html',
+                            controller: 'HistoryListController'
+                        })
+                        .when('/createHistory', {
+                            templateUrl: 'partials/history/create.html',
+                            controller: 'HistoryCreateController'
+                        })
+                        .when('/rootResource', {
+                            templateUrl: 'partials/rootResource/index.html',
+                            controller: 'RootResourceController'
+                        }).when('/info/:id', {
+                    templateUrl: 'partials/rootResource/info.html',
+                    controller: 'InfoRootResourceController'
+                })
+                        .when('/listServices/:id', {
+                            templateUrl: 'partials/serviceProvider/info.html',
+                            controller: 'serviceProviderController'
+                        })
+                        .when('/rootResource/create', {
+                            templateUrl: 'partials/rootResource/info.html',
+                            controller: 'CreateRootResourceController'
+                        })
+                        .when('/tree', {
+                            templateUrl: 'partials/tree.html',
+                            controller: 'treeCtrl'
+                        })
+                        .when('/infoResource/:id', {
+                            templateUrl: 'partials/mqnaasResource/info.html',
+                            controller: 'MqNaaSResourceController'
+                        })
+                        .when('/sodales', {
+                            templateUrl: 'partials/sodales/index.html',
+                            controller: 'SodalesController'
+                        })
+                        .when('/sodales_statistics', {
+                            templateUrl: 'partials/sodales/statistics.html',
+                            controller: 'SodalesStatisticsController'
+                        })
+                        .when('/graph', {
+                            templateUrl: 'partials/graph.html'
+                        })
+                        .when('/doAction/:action', {
+                            controller: 'ContentController'
+                        })
+                        .when('/crawler', {
+                            template: " ", // just fire controller
+                            controller: 'CrawlerResourceController',
+                            animation: 'slide'
+                        })
+                        .otherwise({
+                            templateUrl: 'partials/index.html',
+                            //controller: IndexController
+                        });
 
-                        $locationProvider.hashPrefix('!');
+                $locationProvider.hashPrefix('!');
 
-                        /* Register error provider that shows message on failed requests or redirects to login page on
-                         * unauthenticated requests */
-                        $httpProvider.interceptors.push(function ($q, $rootScope, $location) {
-                            return {
-                                'responseError': function (rejection) {
-                                    var status = rejection.status;
-                                    var config = rejection.config;
-                                    var method = config.method;
-                                    var url = config.url;
+                /* Register error provider that shows message on failed requests or redirects to login page on
+                 * unauthenticated requests */
+                $httpProvider.interceptors.push(function ($q, $rootScope, $location) {
+                    return {
+                        'responseError': function (rejection) {
+                            var status = rejection.status;
+                            var config = rejection.config;
+                            var method = config.method;
+                            var url = config.url;
 
-                                    if (status == 401) {
-                                        $location.path("/login");
-                                    } else {
-                                        $rootScope.error = method + " on " + url + " failed with status " + status;
-                                    }
+                            if (status == 401) {
+                                $location.path("/login");
+                            } else {
+                                $rootScope.error = method + " on " + url + " failed with status " + status;
+                            }
 
-                                    return $q.reject(rejection);
-                                }
-                            };
+                            return $q.reject(rejection);
                         }
-                        );
+                    };
+                }
+                );
 
-                        /* Registers auth token interceptor, auth token is either passed by header or by query parameter
-                         * as soon as there is an authenticated user */
-                        $httpProvider.interceptors.push(function ($q, $rootScope, $location) {
-                            return {
-                                'request': function (config) {
-                                    var isRestCall = config.url.indexOf('rest') == 0;
-                                    if (isRestCall && angular.isDefined($rootScope.authToken)) {
-                                        var authToken = $rootScope.authToken;
-                                        if (openNaaSAppConfig.useAuthTokenHeader) {
-                                            config.headers['X-Auth-Token'] = authToken;
-                                        } else {
-                                            config.url = config.url + "?token=" + authToken;
-                                        }
-                                    }
-                                    return config || $q.when(config);
+                /* Registers auth token interceptor, auth token is either passed by header or by query parameter
+                 * as soon as there is an authenticated user */
+                $httpProvider.interceptors.push(function ($q, $rootScope, $location) {
+                    return {
+                        'request': function (config) {
+                            var isRestCall = config.url.indexOf('rest') == 0;
+                            if (isRestCall && angular.isDefined($rootScope.authToken)) {
+                                var authToken = $rootScope.authToken;
+                                if (openNaaSAppConfig.useAuthTokenHeader) {
+                                    config.headers['X-Auth-Token'] = authToken;
+                                } else {
+                                    config.url = config.url + "?token=" + authToken;
                                 }
-                            };
+                            }
+                            return config || $q.when(config);
                         }
-                        );
+                    };
+                }
+                );
 
-                    }]
+            }]
 
-                ).run(function ($rootScope, $location, $cookieStore, UserService) {
+        ).run(function ($rootScope, $location, $cookieStore, UserService) {
 
     /* Reset error when a new view is loaded */
     $rootScope
-    .$on('$viewContentLoaded', function () {
-        delete $rootScope.error;
-    });
+            .$on('$viewContentLoaded', function () {
+                delete $rootScope.error;
+            });
 
     $rootScope.hasRole = function (role) {
 
@@ -169,45 +177,6 @@ angular.module('openNaaSApp', ['ngResource', 'ngRoute', 'ngCookies', 'openNaaSAp
 
     $rootScope.initialized = true;
 });
-
-/*
-function IndexController($scope, NewsService) {
-
-    $scope.newsEntries = NewsService.query();
-
-    $scope.deleteEntry = function (newsEntry) {
-        newsEntry.$remove(function () {
-            $scope.newsEntries = NewsService.query();
-        });
-    };
-}
-;
-
-function EditController($scope, $routeParams, $location, NewsService) {
-
-    $scope.newsEntry = NewsService.get({id: $routeParams.id});
-
-    $scope.save = function () {
-        $scope.newsEntry.$save(function () {
-            $location.path('/');
-        });
-    };
-}
-;
-
-
-function CreateController($scope, $location, NewsService) {
-
-    $scope.newsEntry = new NewsService();
-
-    $scope.save = function () {
-        $scope.newsEntry.$save(function () {
-            $location.path('/');
-        });
-    };
-}
-;
-*/
 
 var services = angular.module('openNaaSApp.services', ['ngResource']);
 var genericUrl = "rest/mqnaas/";
