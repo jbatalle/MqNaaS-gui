1fb1eef6a36758a1edccd6436940d736d250b307
Josep <jbatalle3@gmail.com>
2014-12-24 14:23:35 +0100

    Tree-test commented some log

--- src/test/javascript/spec/controllers/Tree_test.js
+++ src/test/javascript/spec/controllers/Tree_test.js
@@ -24,33 +24,12 @@ var tree = new TreeModel(),
     });
     var node = tree.parse({name: 'port-2'});
     nodeTSON.addChild(node);
-    console.log("Before");
-    console.log(root);
-    console.log(root.isRoot());
-    console.log(root.children);
-    console.log(nodeTSON);
-    console.log(node);
     var portNode = root.first(function(node){
         return node.model.name === "port-2";
     });
-    console.log(portNode.model);
-    console.log(portNode.isRoot());
-    console.log(portNode.getPath());
-    var arr = portNode.getPath();
-    for(var i = 0; i < arr.length; i++){
-        console.log(arr[i].children);
-        console.log(arr[i].model);
-        console.log(arr[i].model.name);
-    }
-    console.log(root);
     
     node = tree.parse({name: 'port-2'});
     nodeTSON.addChild(node);
-    for(var i = 0; i < nodeTSON.children.length; i++){
-        console.log(nodeTSON.children[i].children);
-        console.log(nodeTSON.children[i].model);
-        console.log(nodeTSON.children[i].model.name);
-    }
     
         expect(scope.tableParams.data.length).toBe(0);
 //            expect(scope.tableParams.data.length).toEqual(1);
