71652b33cda18cdeb14e914af5ec11dc7bac29d3
Josep <jbatalle3@gmail.com>
2014-12-24 10:33:11 +0100

    Starting implementation of openaaccess controllers

--- src/main/webapp/js/controllers/sodales/sodalesOpenaccessDashCtrl.js
+++ src/main/webapp/js/controllers/sodales/sodalesOpenaccessDashCtrl.js
@@ -1,7 +1,51 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+'use strict';
 
+angular.module('openNaaSApp')
+        .controller('SodalesOpenaccessDashCtrl', function ($scope, $filter, ngTableParams, spService, ngDialog) {
 
+$scope.listVi = [{name:"vi-1"},{name:"vi-2"},{name:"vi-4"}];
+            spService.list().then(function (data) {
+                console.log(data);
+                //$scope.spList = data;
+                $scope.tableParams = new ngTableParams({
+                    page: 1, // show first page
+                    count: 10, // count per page
+                    sorting: {
+                        date: 'desc'     // initial sorting
+                    }
+                }, {
+                    total: data.length,
+                    getData: function ($defer, params) {
+                        console.log(data.length);
+                        var orderedData = params.sorting() ? $filter('orderBy')(data, params.orderBy()) : data;
+                        $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+                    },
+                    $scope: {$data: {}}
+                });
+            });
+
+            $scope.openSPCreationDialog = function () {
+                ngDialog.open({template: 'partials/sodales/spCreationDialog.html'});
+            };
+            
+            $scope.addVI = function (spName, vi) {
+                console.log(spName);
+                console.log(vi);
+                spService.addViToSP(spName, vi.name).then(function (response) {
+                    
+                });
+
+            };
+            
+            $scope.addSP = function (data) {
+                console.log("Adding SP");
+                console.log(data);
+                var name = data.name;//{"name":"SP4"}
+                var sp = {"name": name.replace( /\s/g, "")};
+                console.log(sp);
+                spService.createSP(sp).then(function (response) {
+                    console.log(response);
+                });
+                ngDialog.close();
+            };
+        });
--- src/main/webapp/js/controllers/sodales/sodalesOpenaccesshistoryCtrl.js
+++ src/main/webapp/js/controllers/sodales/sodalesOpenaccesshistoryCtrl.js
@@ -1,7 +1,22 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+'use strict';
 
+angular.module('openNaaSApp')
+        .controller('SodalesHistoryController', function ($scope, HistoryService, $filter, ngTableParams) {
+            var data = HistoryService.query({}, function () {
+                $scope.tableParams.reload();
+            });
 
+            $scope.tableParams = new ngTableParams({
+                page: 1, // show first page
+                count: 10, // count per page
+                sorting: {
+                    date: 'desc'     // initial sorting
+                }
+            }, {
+                total: data.length,
+                getData: function ($defer, params) {
+                    var orderedData = params.sorting() ? $filter('orderBy')(data, params.orderBy()) : data;
+                    $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+                }, $scope: {$data: {}}
+            });
+        });
